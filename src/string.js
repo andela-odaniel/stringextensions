c1f9fcead045896c0b4220bc9fca5930d05efa84
redd <danielonodje@gmail.com>
2016-10-23 17:52:59 +0100

    add method to convert the string to solely upper case

--- src/string.js
+++ src/string.js
@@ -9,3 +9,32 @@ String.prototype.hasVowels = function() {
     const matchRegex = /[aeiouAEIOU]/g;
     return matchRegex.test(this);
 };
+
+/**
+ * Converts the string to solely upper case
+ * 
+ * @returns {String}
+ */
+String.prototype.toUpper = function() {
+    let input = this;
+    const matchRegex = /[a-z]/g;
+    let lowerCaseCharacters = input.match(matchRegex);
+
+    if(lowerCaseCharacters){
+        //remove duplicates
+        lowerCaseCharacters = lowerCaseCharacters
+            .filter((character,index) => {
+                return lowerCaseCharacters.indexOf(character) === index; 
+            })
+            .map((character) => {
+                let asciiValue = character.charCodeAt(0),
+                    //there's a distance of 32 characters between upper case and lower case ascii characters
+                    upperCaseAsciiValue = asciiValue - 32,
+                    uppercaseStringValue = String.fromCharCode(upperCaseAsciiValue);
+                    //replace all occurences of the character with the upper case
+                 input = input.replace(new RegExp(character,"g"),uppercaseStringValue);
+        })
+    }
+
+    return input;
+};
--- test/test.js
+++ test/test.js
@@ -14,4 +14,14 @@ describe('String Extensions',function(){
             expect(word2.hasVowels()).to.be.false;
         })
     })
+
+    describe('.toUpper()', function(){
+        it('should convert a string containing lower case characters to upper case only',function(){
+            let word1 = "this is a string";
+            expect(word1.toUpper()).to.equal('THIS IS A STRING');
+
+            let word2 = "This Is A string";
+            expect(word2.toUpper()).to.equal('THIS IS A STRING');
+        })
+    })
 });
